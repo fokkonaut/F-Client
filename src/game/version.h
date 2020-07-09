4391c54c756f3f524842760d6bec033e697d72c2
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2020-07-09 13:39:08 +0200

    Update to 2.8

--- src/game/version.h
+++ src/game/version.h
@@ -2,10 +2,10 @@
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
 #ifndef GAME_VERSION_H
 #define GAME_VERSION_H
-#define GAME_VERSION "0.7.5, 2.5"
+#define GAME_VERSION "0.7.5, 2.8"
 #define GAME_NETVERSION "0.7 802f1be60a05665f"
-#define CLIENT_VERSION 0x0f2050
-#define PREV_CLIENT_VERSION 0x0f2050
+#define CLIENT_VERSION 0x0f2080
+#define PREV_CLIENT_VERSION 0x0f2070
 #define SETTINGS_FILENAME "settings_fclient"
 #define DDRACE_VERSION 1
 #define FCLIENT_STRING "fclient!"

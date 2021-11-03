20a053ebef70c4c6cb6e17da268038ad7bff2f14
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2021-11-03 21:59:16 +0100

    Update to 3.3, fix ddnet version --> fix open doors when closed

--- src/game/version.h
+++ src/game/version.h
@@ -2,11 +2,11 @@
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
 #ifndef GAME_VERSION_H
 #define GAME_VERSION_H
-#define GAME_VERSION "0.7.5, 3.2"
+#define GAME_VERSION "0.7.5, 3.3"
 #define GAME_NETVERSION "0.7 802f1be60a05665f"
-#define CLIENT_VERSION 0x0f3020
-#define PREV_CLIENT_VERSION 0x0f3010
-#define CLIENT_DDNET_VERSION 15060
+#define CLIENT_VERSION 0x0f3030
+#define PREV_CLIENT_VERSION 0x0f3020
+#define CLIENT_DDNET_VERSION 15050
 #define GAME_NAME "F-Client"
 #define SETTINGS_FILENAME "settings_fclient"
 #define FCLIENT_STRING "fclient!"

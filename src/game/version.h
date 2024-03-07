5a326e434ceeca4012ae7146b75589c9c69f639a
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2024-03-07 22:14:56 +0100

    Version 3.3.1

--- src/game/version.h
+++ src/game/version.h
@@ -2,10 +2,10 @@
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
 #ifndef GAME_VERSION_H
 #define GAME_VERSION_H
-#define GAME_VERSION "0.7.5, 3.3"
+#define GAME_VERSION "0.7.5, 3.3.1"
 #define GAME_NETVERSION "0.7 802f1be60a05665f"
-#define CLIENT_VERSION 0x0f3030
-#define PREV_CLIENT_VERSION 0x0f3020
+#define CLIENT_VERSION 0x0f3031
+#define PREV_CLIENT_VERSION 0x0f3030
 #define CLIENT_DDNET_VERSION 15050
 #define GAME_NAME "F-Client"
 #define SETTINGS_FILENAME "settings_fclient"
